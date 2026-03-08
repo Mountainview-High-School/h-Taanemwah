b5a4108508b8f7cfc5c90ad01447a2e826bab8eb
Taanemwah <23mcintosht@mountainview.school.nz>
2026-03-09 12:18:01 +1300

    numba 8

--- -- SQLite.sql
+++ -- SQLite.sql
@@ -23,3 +23,7 @@ select c.teacher_id, c.room_id from class c
 inner join Teacher t
 on c.teacher_id = t.teacher_id
 WHERE c.room_id in (44, 43);
+
+-- 8
+SELECT student_id, first_name, last_name FROM Student
+WHERE student_id IN (SELECT student_id FROM Class WHERE room_id = 44);
